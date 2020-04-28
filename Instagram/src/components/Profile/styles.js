75f273df7132bb14d975ea15d0a55eed52135995
mjsil <m.j.silveira@outlook.com>
2020-04-27 22:35:33 -0300

    Profile

---
+++ Instagram/src/components/Profile/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const ImageProfile = styled.Image`
+    height: 25px;
+    width: 25px;
+    border-radius: 12.5px;
+`;
+
+export const FundoImagem = styled.View`
+    /* background: #fff;
+    height: 26px;
+    width: 26px;
+    border-radius: 13px;
+    align-items: center;
+    justify-content: center; */
+`;
+
+export const FundoImagemFalse = styled.View`
+    /* background: #fff;
+    height: 24px;
+    width: 24px;
+    border-radius: 12px;
+    align-items: center;
+    justify-content: center; */
+`;
